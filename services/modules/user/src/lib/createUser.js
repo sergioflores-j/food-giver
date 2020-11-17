43347214d334d2c9ac489f989375d64a2486b8a8
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2020-11-16 21:57:13 -0300

    feat: create user object schema

---
+++ services/modules/user/src/lib/createUser.js
@@ -0,0 +1,50 @@
+// @ts-check
+import { error } from '@shared/utils/utils';
+import { isObject } from 'deep-object-js';
+import env from '@root/ms.env';
+
+import UserDao from '@shared/dao/UserDao';
+import userSchema from '@shared/schemas/user';
+
+/**
+ * @param {object} param0
+ * @param {import('../../../../../types/User').User} param0.user
+ */
+export const create = async ({ user }) => {
+  checkParameters({ user });
+
+  await run({ user });
+};
+
+export const run = async ({ user }) => {
+  try {
+    const userItem = {
+      ...user,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+
+    await userSchema.validate(userItem, { abortEarly: false });
+
+    await new UserDao().create({ user: userItem });
+
+    return { user: userItem };
+  } catch (err) {
+    console.log('Error CreateUser Run', err);
+    console.log('Params: ', {
+      user,
+    });
+    throw err;
+  }
+};
+
+const checkParameters = ({ user }) => {
+  const errors = {};
+
+  if (!user) errors.user = 'undefined';
+  else if (!isObject(user)) errors.user = 'is not an object';
+
+  if (Object.keys(errors).length) throw error(env.STATUS_BAD_REQUEST, errors);
+};
+
+export default create;
