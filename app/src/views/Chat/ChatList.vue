c8f5eb1ff8172bb9444d921ccd927d34aa01d2f6
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2020-11-28 15:44:25 -0300

    feat: chat list page

--- app/src/views/Chat/ChatList.vue
+++ app/src/views/Chat/ChatList.vue
@@ -1,63 +1,109 @@
 <template>
-  <v-card>
+  <v-card
+    class="mx-auto"
+    max-width="500"
+    :loading="loading.list"
+  >
     <v-card-title>
       Meus chats
-      <v-spacer />
     </v-card-title>
+
+    <v-list subheader>
+      <v-list-item
+        v-for="chat in list"
+        :key="chat.chatId"
+        :to="`/chats/${chat.chatId}`"
+      >
+        <v-list-item-avatar
+          :color="getRandomColor()"
+          rounded
+          left
+        >
+          {{ getEmailInitials(chat) }}
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="getOtherParticipantEmail(chat)" />
+        </v-list-item-content>
+
+        <v-list-item-icon>
+          <v-icon :color="'grey'">
+            mdi-message-outline
+          </v-icon>
+        </v-list-item-icon>
+      </v-list-item>
+    </v-list>
+    <v-snackbar
+      v-model="showErrorSnackbar"
+      light
+    >
+      Não foi possível obter seus chats, tente novamente mais tarde!
+
+      <template #action="{ attrs }">
+        <v-btn
+          color="red"
+          text
+          v-bind="attrs"
+          @click="showErrorSnackbar = false"
+        >
+          Fechar
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-card>
 </template>
-
 <script>
-// import { listByUserEmail } from '@/services/donation';
-// import { formatDateTime } from '@/utils/formatters';
-// import { conditions } from '@/constants/donation';
+import { listByUserEmail } from '@/services/chat';
+import colors from 'vuetify/lib/util/colors';
 
 export default {
   name: 'ChatListView',
-  // data() {
-  //   return {
-  //     singleSelect: false,
-  //     selected: [],
-  //     search: '',
-  //     headers: [
-  //       { text: 'Comida', value: 'foodName' },
-  //       { text: 'Condição', value: 'condition' },
-  //       { text: 'Criação', value: 'createdAt' },
-  //       { text: 'Última Atualização', value: 'updatedAt' },
-  //     ],
-  //     list: [],
-  //     isLoading: {
-  //       list: false,
-  //     },
-  //     showErrorSnackbar: false,
-  //   };
-  // },
-  // mounted() {
-  //   this.getDonationList();
-  // },
-  // methods: {
-  //   formatDateTime,
-  //   async getDonationList() {
-  //     this.list = [];
+  data() {
+    return {
+      list: [],
+      loading: {
+        list: false,
+      },
+      showErrorSnackbar: false,
+    };
+  },
+  computed: {
+    sessionUser() {
+      return this.$store.state.auth.user;
+    },
+  },
+  mounted() {
+    this.getChatList();
+  },
+  methods: {
+    getRandomColor() {
+      const colorsKeys = Object.keys(colors);
+      const [firstKey] = colorsKeys.sort(() => 0.5 - Math.random());
 
-  //     if (this.isLoading.list) return;
+      return colors[firstKey].base;
+    },
+    getEmailInitials({ participant1, participant2 } = {}) {
+      return this.getOtherParticipantEmail({ participant1, participant2 }).substr(0, 1).toUpperCase();
+    },
+    getOtherParticipantEmail({ participant1, participant2 } = {}) {
+      if (participant1 === this.sessionUser.email) return participant2;
+      return participant1;
+    },
+    async getChatList() {
+      this.list = [];
 
-  //     this.isLoading.list = true;
-  //     try {
-  //       const { donations } = await listByUserEmail();
-  //       this.list = donations;
-  //     } catch (error) {
-  //       this.showErrorSnackbar = true;
-  //     } finally {
-  //       this.isLoading.list = false;
-  //     }
-  //   },
-  //   getConditionLabel(condition) {
-  //     return conditions[condition].label || '';
-  //   },
-  //   getConditionColor(condition) {
-  //     return conditions[condition].color || '';
-  //   },
-  // },
+      if (this.loading.list) return;
+
+      this.loading.list = true;
+      try {
+        const { chats } = await listByUserEmail();
+        this.list = chats;
+      } catch (error) {
+        this.showErrorSnackbar = true;
+      } finally {
+        this.loading.list = false;
+      }
+    },
+  },
 };
 </script>
